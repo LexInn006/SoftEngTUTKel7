8cae0b50a86029cfcabc87d8216126901776eab6
LexInn006 <marcoalexius09@gmail.com>
2026-05-25 18:12:57 +0700

    update_rules_test
    testing kasih syarat. umur gabisa negatif

--- src/rules.py
+++ src/rules.py
@@ -15,4 +15,7 @@ def compute_room_price(
     stay_duration,
 ):
     
-    return
+    if guest_age < 0:
+        raise ValueError("Invalid age: Age cannot be negative.")
+    
+    return int(base_price * stay_duration)
