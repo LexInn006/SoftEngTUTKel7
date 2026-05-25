bdbb1b6c1b8961a1194611dc0dfe39ca3dffa760
deindsia-web <devanoindonesia@gmail.com>
2026-05-25 20:16:34 +0700

    Update Rules
    Testing

--- test/test_room_price.py
+++ test/test_room_price.py
@@ -2,13 +2,13 @@ import pytest
 from src.rules import compute_room_price
 
 
-# def test_guest_adult():
-#     assert compute_room_price(
-#         guest_age=34,
-#         room_type="standard",
-#         booking_day="weekday",
-#         stay_duration=3
-#     ) == 2700000
+def test_guest_adult():
+    assert compute_room_price(
+        guest_age=34,
+        room_type="standard",
+        booking_day="weekday",
+        stay_duration=3
+    ) == 2700000
 
 
 
@@ -32,3 +32,6 @@ def test_invalid_age():
 #         booking_day="holiday",
 #         stay_duration=5
 #     ) == 0
+
+
+
